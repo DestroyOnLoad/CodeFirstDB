f2cd52d2d8309256ca17b9226008483c649b43f1
David Potter <destroyonload@gmail.com>
2020-12-21 10:12:21 +0800

    init commit Created Model and Migrated to DB

---
+++ CodeFirstDesign/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirstDesign.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstDesign.Program.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstDesign.Program.PlutoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
